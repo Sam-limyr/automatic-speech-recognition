2db4b807db9b2104a3cf8a58d1dab8ca19f439f2
xunwei <yeexunwei@gmail.com>
2021-10-23 00:16:48 +0800

    Add methods to check srt timing accuracy

--- scraper/preprocess.py
+++ scraper/preprocess.py
@@ -90,23 +90,54 @@ def save_trans(transcript, output_path):
                 f.write(f"{line}\n")
             f.close()
 
+def check_ms_accuracy(time_slices, occurrence_threshold=20):
+    for time_slice in time_slices:
+        occurrence_820 = sum(1 for elem in time_slices if elem[1][0] % 1000 == 820)
+    if occurrence_820 > 20:
+        return occurrence_820
+    return 0
+
+def check_intro_timing(audio_duration, audio_srt_duration):
+    duration_diff = audio_duration - audio_srt_duration
+    if duration_diff > 7:
+        return duration_diff
+    return 0
+
+
 def convert(src_path=src_path):
+    shutil.rmtree(join(path, split), ignore_errors=True)
     folder_list = os.listdir(src_path)
     for idx, curr_folder in enumerate(folder_list):
-        file_name = str(idx) #save the transcript as idx, can be changed to folder name
+        file_name = str(idx) #save the transcript as num, can be changed to folder name
         output_path = join(path, split, file_name)
         txt_output_path = join(output_path, file_name + '.trans.txt')
         
-        # text
         logging.info(f"{idx}. Creating transcript for {curr_folder}...")
         txt_path = list(Path(join(src_path, curr_folder)).rglob('*.txt'))[0]
         transcript, time_slices = txt_to_trans(txt_path, file_name)
-        save_trans(transcript, txt_output_path)
         
-        # audio
         logging.info(f"{idx}. Slicing audio for {curr_folder}...")
         audio_path = list(Path(join(src_path, curr_folder)).rglob('*.' + AUDIO_EXTENSION))[0]
         audio_file = AudioSegment.from_file(audio_path, AUDIO_EXTENSION)
+        
+        # check whether srt is accurate, remove those not accurate to milliseconds (all time that starts with __,820ms)
+        ms_not_accurate = check_ms_accuracy(time_slices)
+        if ms_not_accurate:
+            logging.warning(f"{idx}. Likely srt not accurate with {ms_not_accurate} 820s. Deleting foler {output_path}")
+            shutil.rmtree(output_path, ignore_errors=True)
+            continue
+        
+        # check whether srt matches audio, remove those without taking water drop intro into account
+        audio_duration = audio_file.duration_seconds
+        audio_srt_duration = time_slices[-1][-1][-1] / 1000
+        intro_not_matched = check_intro_timing(audio_duration, audio_srt_duration)
+        if intro_not_matched:
+            logging.warning(f"{idx}. Likely srt not matching with time slices. Deleting foler {output_path}")
+            shutil.rmtree(output_path, ignore_errors=True)
+            continue
+        
+        # writing output
+        save_trans(transcript, txt_output_path)
         for idx, time_slice in time_slices:
             audio_slice = audio_file[time_slice[0]:time_slice[1]]
             audio_output_path = join(output_path, f"{file_name}-{idx}.{AUDIO_EXTENSION}")
