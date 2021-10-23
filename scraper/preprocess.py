67e4c7ba06590fb0b0a01cfdf7993e257b554611
xunwei <yeexunwei@gmail.com>
2021-10-24 00:09:35 +0800

    Add lines to create normalized-lm-text.txt

--- scraper/preprocess.py
+++ scraper/preprocess.py
@@ -2,6 +2,7 @@ import os
 from joblib import Parallel, delayed
 from os.path import join
 from pathlib import Path
+import shutil
 
 import scraper
 import re
@@ -9,11 +10,14 @@ import subprocess
 import num2words
 import pydub
 from pydub import AudioSegment
+from tqdm import tqdm
 import logging
 
 logger = logging.getLogger()
-logging.basicConfig(level="INFO", format="%(levelname)s: %(filename)s: %(message)s")
-
+# logging.basicConfig(level="INFO", format="%(levelname)s: %(filename)s: %(message)s")
+# logging.basicConfig(level='WARNING', format="%(levelname)s: %(filename)s: %(message)s")
+# logger.setLevel(logging.WARNING)
+logger.disabled = True
 
 AUDIO_EXTENSION = 'mp3'
 READ_FILE_THREADS = 1
@@ -22,6 +26,9 @@ split = 'train'
 
 src_path = 'data'
 
+
+DURATION_DIFF = 7 # seconds
+REPEATED_OCCURRENCE = 50
 
 def clean_text(text):
     '''
@@ -32,6 +39,17 @@ def clean_text(text):
     text = re.sub(r'[^\w\s]', ' ', text)
     text = ' '.join([num2words.num2words(i, to='year') if (i.isdigit() & (len(i) == 4)) else i for i in text.split()]) # year to words
     text = ' '.join([num2words.num2words(i) if i.isdigit() else i for i in text.split()]) # num to words
+    text = re.sub(' +', ' ', text) # remove redundant spaces
+    text = text.replace('-', ' ')
+    return text
+
+def normalize_text(text):
+    '''
+    Text processing to normalize text for language model training
+    should transform word numeral to numeric, normalize date formats, lemmatization; most not implemented currently
+    '''
+    text = text.lower().strip('\n')
+    text = re.sub(r'[^\w\s]', ' ', text)
     text = re.sub(' +', ' ', text) # remove redundant spaces
     text = text.replace('-', ' ')
     return text
@@ -49,7 +67,7 @@ def to_ms(string):
     second = second
     return second
 
-def txt_to_trans(txt_file, file_name, text_processing=clean_text):
+def txt_to_trans(txt_file, file_name):
     '''
     Convert txt file to transcript format ready to be read into Dataset
     lines formatted as 'filename-idx text_label'
@@ -59,7 +77,8 @@ def txt_to_trans(txt_file, file_name, text_processing=clean_text):
     lines = file.readlines()
     file.close()
     
-    transcript = []
+    transcript = [] # label for audio
+    txt_src = [] # label for language model
     time_slices = []
 
     for i in range(len(lines)):
@@ -71,12 +90,15 @@ def txt_to_trans(txt_file, file_name, text_processing=clean_text):
                 start, end = to_ms(time_frame[0]), to_ms(time_frame[1])
                 time_slices.append((idx, (start, end)))
 
-                text = lines[i+2]
-                text = text_processing(text)
-                new_line = f"{file_name}-{idx} {text}"
+                audio_label = lines[i+2]
+                audio_label = clean_text(audio_label)
+                new_line = f"{file_name}-{idx} {audio_label}"
                 transcript.append(new_line)
                 
-    return transcript, time_slices
+                lm_label = normalize_text(audio_label)
+                txt_src.append(lm_label)
+                
+    return transcript, time_slices, txt_src
 
 def save_trans(transcript, output_path):
     '''
@@ -85,45 +107,45 @@ def save_trans(transcript, output_path):
     if not os.path.exists(os.path.dirname(output_path)):
         os.makedirs(os.path.dirname(output_path))
 
-        with open(output_path, 'w+') as f:
-            for line in transcript:
-                f.write(f"{line}\n")
-            f.close()
+    with open(output_path, 'a') as f:
+        for line in transcript:
+            f.write(f"{line}\n")
+        f.close()
 
 def check_ms_accuracy(time_slices, occurrence_threshold=20):
     for time_slice in time_slices:
         occurrence_820 = sum(1 for elem in time_slices if elem[1][0] % 1000 == 820)
-    if occurrence_820 > 20:
+    if occurrence_820 > REPEATED_OCCURRENCE:
         return occurrence_820
     return 0
 
 def check_intro_timing(audio_duration, audio_srt_duration):
     duration_diff = audio_duration - audio_srt_duration
-    if duration_diff > 7:
+    if duration_diff > DURATION_DIFF:
         return duration_diff
     return 0
-
 
 def convert(src_path=src_path):
     shutil.rmtree(join(path, split), ignore_errors=True)
     folder_list = os.listdir(src_path)
-    for idx, curr_folder in enumerate(folder_list):
+    for idx, curr_folder in enumerate(tqdm(folder_list, desc="Looping over folders")):
+        logging.info('\n')
         file_name = str(idx) #save the transcript as num, can be changed to folder name
         output_path = join(path, split, file_name)
         txt_output_path = join(output_path, file_name + '.trans.txt')
         
         logging.info(f"{idx}. Creating transcript for {curr_folder}...")
         txt_path = list(Path(join(src_path, curr_folder)).rglob('*.txt'))[0]
-        transcript, time_slices = txt_to_trans(txt_path, file_name)
+        transcript, time_slices, txt_src = txt_to_trans(txt_path, file_name)
         
         logging.info(f"{idx}. Slicing audio for {curr_folder}...")
         audio_path = list(Path(join(src_path, curr_folder)).rglob('*.' + AUDIO_EXTENSION))[0]
         audio_file = AudioSegment.from_file(audio_path, AUDIO_EXTENSION)
         
-        # check whether srt is accurate, remove those not accurate to milliseconds (all time that starts with __,820ms)
+        # check whether srt is accurate, remove those not accurate to milliseconds, check repeated occurrence of 820
         ms_not_accurate = check_ms_accuracy(time_slices)
         if ms_not_accurate:
-            logging.warning(f"{idx}. Likely srt not accurate with {ms_not_accurate} 820s. Deleting foler {output_path}")
+            logging.warning(f"{idx}. Srt not accurate with {ms_not_accurate} 820s. Deleting entry {curr_folder}")
             shutil.rmtree(output_path, ignore_errors=True)
             continue
         
@@ -132,19 +154,26 @@ def convert(src_path=src_path):
         audio_srt_duration = time_slices[-1][-1][-1] / 1000
         intro_not_matched = check_intro_timing(audio_duration, audio_srt_duration)
         if intro_not_matched:
-            logging.warning(f"{idx}. Likely srt not matching with time slices. Deleting foler {output_path}")
+            logging.warning(f"{idx}. Srt not matching with time slices. Deleting entry {curr_folder}")
             shutil.rmtree(output_path, ignore_errors=True)
             continue
         
         # writing output
         save_trans(transcript, txt_output_path)
+        save_trans(txt_src, txt_src_path)
         for idx, time_slice in time_slices:
             audio_slice = audio_file[time_slice[0]:time_slice[1]]
             audio_output_path = join(output_path, f"{file_name}-{idx}.{AUDIO_EXTENSION}")
             audio_slice.export(audio_output_path, format=AUDIO_EXTENSION)
+        
+        tqdm.write(f'Successfully created {idx} {curr_folder}')
+        # print(f'Successfully created {idx} {curr_folder}')
 
 def main():
-    scraper.main()
+    # print('Scraping data')
+    # scraper.main(number_of_talks=200, starting_video_id=100)
+    print()
+    print('Start preprocessing')
     convert(src_path)
 
 
